fa7069fcffe541ea694473188baf269438a87334
Jeremy Howard <j@fast.ai>
2019-12-11 15:18:14 -0800

    imports

--- fastcore/imports.py
+++ fastcore/imports.py
@@ -15,6 +15,7 @@ from operator import itemgetter,attrgetter
 # External modules
 import numpy as np,pandas as pd
 from numpy import array,ndarray
+from pandas import DataFrame
 from IPython.core.debugger import set_trace
 
 #Optional modules

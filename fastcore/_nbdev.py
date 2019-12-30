c2dbb42bf9bcca4075e44ad1ec8ac6314b7cd443
Sylvain Gugger <sylvain.gugger@gmail.com>
2019-12-30 06:13:47 -0800

    New doc attirbutes

--- fastcore/_nbdev.py
+++ fastcore/_nbdev.py
@@ -149,7 +149,7 @@ modules = ["test.py",
            "dispatch.py",
            "transform.py"]
 
-doc_url = "http:/fastcore.fast.ai/"
+doc_url = "https://fastai.github.io/fastcore/"
 
 git_url = "https://github.com/fastai/fastcore/tree/master/"
 

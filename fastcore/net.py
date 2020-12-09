c66cd1b992ca95c7901ce00dba5ad047375764ab
Jeremy Howard <j@fast.ai>
2020-12-09 14:54:53 -0800

    remove gzip accept

--- fastcore/net.py
+++ fastcore/net.py
@@ -32,7 +32,7 @@ from http.client import InvalidURL
 # Cell
 url_default_headers = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    "Accept-Encoding": "gzip, deflate, br",
+#     "Accept-Encoding": "gzip, deflate, br",
     "Accept-Language": "en-US,en;q=0.9",
     "Cache-Control": "max-age=0",
     "Sec-Fetch-Dest": "document",

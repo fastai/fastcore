82e374de983e33aaeceb1a8a104d03af286f5090
Jeremy Howard <j@fast.ai>
2021-01-01 15:32:49 -0800

    fixes #271

--- examples/test_fastcore.py
+++ examples/test_fastcore.py
@@ -4,7 +4,7 @@ from fastcore.script import *
 
 @call_parse
 def main(msg:Param("The message", str),
-         upper:Param("Convert to uppercase?", store_true)):
+         upper:Param("Convert to uppercase?", store_true)=False):
     "Print `msg`, optionally converting to uppercase"
     print(msg.upper() if upper else msg)
 

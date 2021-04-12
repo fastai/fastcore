0540e2b36a16d53bcf197310803541f503e0af3c
Yi Zhang <zhanyi@microsoft.com>
2021-04-11 21:13:12 -0700

    add more windows compatibilty for run

--- fastcore/xtras.py
+++ fastcore/xtras.py
@@ -162,8 +162,10 @@ def repo_details(url):
     return res.split('/')[-2:]
 
 # Cell
-def run(cmd, *rest, ignore_ex=False, as_bytes=False, stderr=False):
+def run(cmd, *rest, same_in_win=False, ignore_ex=False, as_bytes=False, stderr=False):
     "Pass `cmd` (splitting with `shlex` if string) to `subprocess.run`; return `stdout`; raise `IOError` if fails"
+    "If the command is same in Windows, we have to add `cmd /c`"
+    if same_in_win: cmd = 'cmd /c' + cmd
     if rest: cmd = (cmd,)+rest
     elif isinstance(cmd,str): cmd = shlex.split(cmd)
     res = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)

89ffc8bdc5a842e297cfc41dfedf0002260ee7c3
Jeremy Howard <github@jhoward.fastmail.fm>
2024-05-15 15:56:55 +1000

    xml tuple

--- fastcore/xml.py
+++ fastcore/xml.py
@@ -13,19 +13,19 @@ __all__ = ['xt', 'to_xml', 'Html', 'Head', 'Title', 'Meta', 'Link', 'Style', 'Bo
 from .utils import *
 
 import types
-from dataclasses import dataclass
 from functools import partial
 from html import escape
 
 # %% ../nbs/11_xml.ipynb 4
 def _attrmap(o):
-    return dict(cls='class', klass='class', fr='for').get(o, o)
+    o = dict(htmlClass='class', cls='class', klass='class', fr='for', htmlFor='for').get(o, o)
+    return o.lstrip('_').replace('_', '-')
 
 # %% ../nbs/11_xml.ipynb 5
 def xt(tag:str, *c, **kw):
     "Create an XML tag structure `[tag,children,attrs]` for `toxml()`"
     if len(c)==1 and isinstance(c[0], types.GeneratorType): c = tuple(c[0])
-    kw = {_attrmap(k.lstrip('_')):str(v) for k,v in kw.items()}
+    kw = {_attrmap(k):str(v) for k,v in kw.items()}
     return [tag.lower(),c,kw]
 
 # %% ../nbs/11_xml.ipynb 6
@@ -45,6 +45,7 @@ for o in _all_: _g[o] = partial(xt, o.lower())
 # %% ../nbs/11_xml.ipynb 9
 def to_xml(elm, lvl=0):
     "Convert `xt` element tree into an XML string"
+    if isinstance(elm, tuple): return '\n'.join(to_xml(o) for o in elm)
     if hasattr(elm, '__xt__'): elm = elm.__xt__()
     sp = ' ' * lvl
     if not isinstance(elm, list):
@@ -54,11 +55,11 @@ def to_xml(elm, lvl=0):
     tag,cs,attrs = elm
     stag = tag
     if attrs:
-        sattrs = (f'{k}="{escape(str(v))}"' for k,v in attrs.items())
+        sattrs = (f'{k}="{escape(str(v), quote=False)}"' for k,v in attrs.items())
         stag += ' ' + ' '.join(sattrs)
     
     if not cs: return f'{sp}<{stag}></{tag}>\n'
     res = f'{sp}<{stag}>\n'
-    res += ''.join(to_xml(c, lvl+2) for c in cs)
+    res += ''.join(to_xml(c, lvl=lvl+2) for c in cs)
     res += f'{sp}</{tag}>\n'
     return res

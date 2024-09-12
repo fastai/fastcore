3b9f27f462a71e65d06f8a1b92b5f15da00f499a
Hamel Husain <hamel.husain@gmail.com>
2024-09-12 15:38:11 -0700

    update llms_ctx files

--- tools/update.sh
+++ tools/update.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-llms_txt2ctx nbs/llms.txt > nbs/llms-ctx-full.txt # https://github.com/answerdotai/llms-txt
-llms_txt2ctx nbs/llms.txt --optional false > nbs/llms-ctx.txt
+llms_txt2ctx nbs/llms.txt --optional true > nbs/llms-ctx-full.txt # https://github.com/answerdotai/llms-txt
+llms_txt2ctx nbs/llms.txt > nbs/llms-ctx.txt
